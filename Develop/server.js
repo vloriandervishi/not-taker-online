81f5ed2c2854141484edc68072b7703cb5258986
Valdo <vlorian22@GMAIL.COM>
2020-12-20 19:30:27 -0600

    installed unique id generator from npm

--- Develop/server.js
+++ Develop/server.js
@@ -1,38 +1,96 @@
-const express = require("express");
-const app = express();
-//const {noteFile}=require("../db/db.json");
-const fs = require("fs");
-const path = require("path");
-//const {noteTitle,noteText}=require('./assets/js/index');
-const PORT = process.env.PORT || 8080;
+const express=require("express");
+const app=express();
+const {noteFile}=require("./db/db.json");
+const fs= require('fs');
+const path= require('path');
 
-app.use(express.urlencoded({ extended: true }));
+//const {noteTitle,noteText}=require('./assets/js/index');
+const PORT= process.env.PORT || 8080;
+
+app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
-app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
+app.use(express.static('public'));
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname , "../public/index.html"));
 });
 
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/notes.html"));
-});
-app.post("/api/notes", (req, res) => {
-  fs.readFileSync(__dirname, "./db/db.json", "UTF-8");
-  newNotes = JSON.parse(newNotes);
-  const applyId = newNotes[newNotes.length - 1].id + 1;
-  const newObjectNote = {
-    title: req.body.title,
-    text: req.body.text,
-    id: applyId,
-  };
-  const hoverNote = newNotes.concat(newObjectNote);
-  fs.writeFileSync(__dirname, "./db/db.json", JSON.stringify(hoverNote));
-  console.log(hoverNote);
-  res.json(hoverNote);
+app.get("/notes",(req,res)=>{
+  res.sendFile(path.join(__dirname ,"./public/notes.html"));
+  
+  });
+
+app.post("/api/notes", function (req, res) {
+  fs.readFile(__dirname + "/db/db.json", 'utf8', function (error, notes) {
+    if (error) {
+      return console.log(error)
+    }
+    notes = JSON.parse(notes)
+
+    var id = notes[notes.length - 1].id + 1
+    var newNote = { title: req.body.title, text: req.body.text, id: id }
+    var activeNote = notes.concat(newNote)
+
+    fs.writeFile(__dirname + "/db/db.json", JSON.stringify(activeNote), function (error, data) {
+      if (error) {
+        return error
+      }
+      console.log(activeNote)
+      res.json(activeNote);
+    })
+  })
+})
+
+// Pull from db.json
+app.get("/api/notes", function (req, res) {
+  fs.readFile(__dirname + "/db/db.json", 'utf8', function (error, data) {
+    if (error) {
+      return console.log(error)
+    }
+    console.log("This is Notes", data)
+    res.json(JSON.parse(data))
+  })
 });
 
-app.listen(PORT, () => {
-  console.log(`Listening on ${PORT}`);
+app.delete("/api/notes/:id", function (req, res) {
+  const noteId = JSON.parse(req.params.id)
+  console.log(noteId)
+  fs.readFile(__dirname + "/db/db.json", 'utf8', function (error, notes) {
+    if (error) {
+      return console.log(error)
+    }
+    notes = JSON.parse(notes)
+
+    notes = notes.filter(val => val.id !== noteId)
+
+    fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), function (error, data) {
+      if (error) {
+        return error
+      }
+      res.json(notes)
+    })
+  })
+})
+
+app.put("/api/notes/:id", function(req, res) {
+  const noteId = JSON.parse(req.params.id)
+  console.log(noteId)
+  fs.readFile(__dirname + "db/db.json", "utf8", function(error, notes) {
+    if (error ){
+      return console.log(error)
+    }
+    notes.JSONparse(notes)
+
+    notes = notes.filter(val => val.id !== noteId)
+
+    fs.writeFile(__dirname +"db/db.json", JSON.stringify(notes), function (error, data) {
+      if (error) {
+        return error
+      }
+      res.json(notes)
+    })
+  })
+})
+app.listen(PORT,()=>{
+ console.log(`Listening on ${PORT}`);
 });
